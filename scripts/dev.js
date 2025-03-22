e98a18585b36d4debcabb7453ff7b857d10c8e23
LorexIQ <dmitrymurashko0@gmail.com>
2025-03-22 18:16:15 +0300

    chore: добавление уровня логирования и обновление скриптов сборки

--- scripts/dev.js
+++ scripts/dev.js
@@ -4,4 +4,5 @@ const { main } = require('../package.json');
 const mainPath = `./dist/${main}`;
 // const args = process.argv.slice(2).join(' ');
 
-execSync(`npm run build:clean && tsc-watch --onSuccess "node ${mainPath}" --onFailure "echo Compilation Failed!"`, { stdio: 'inherit' });
+execSync('npm run build:clean', { stdio: 'inherit' });
+execSync(`tsc-watch --onSuccess "node ${mainPath}" --onFailure "echo Compilation Failed!"`, { stdio: 'inherit' });
